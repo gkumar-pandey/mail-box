3677bcfe8b666020f8cf2744790add92738fd45b
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-01 20:35:42 +0530

    reducers created

---
+++ src/Reducer/index.ts
@@ -0,0 +1,4 @@
+import { mailReducer } from "./MailReducer/mailReducer";
+import { trashReducer } from "./TrashReducer/trashReducer";
+import { spamReducer } from "./SpamReducer/spamReducer";
+export {mailReducer,trashReducer,spamReducer}
