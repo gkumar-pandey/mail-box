18b9622e76b9749e09a0a392b520d3322a5fc70f
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-04 13:52:03 +0530

    delete some useless folders

--- src/Context/SpamContext/SpamContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const SpamContext = createContext<undefined | any>(undefined);
-
-export const SpamContextProvider = ({ children }: any) => {
-  return <SpamContext.Provider value={{}}>{children}</SpamContext.Provider>;
-};
-
-export const useSpam = () => useContext(SpamContext);
--- src/Context/TrashContext/TrashContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const TrashContext = createContext<any | undefined>(undefined);
-
-export const TrashContextProvider = ({ children }: any) => {
-  return <TrashContext.Provider value={{}}>{children}</TrashContext.Provider>;
-};
-
-export const useTrash = () => useContext(TrashContext);
